f8f3d0bb4a763c0473080a07bef21501c1410f55
hintou <jonas.techdeveloper@gmail.com>
2026-03-10 19:11:11 -0300

    update: ajuste dos componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Events from "./pages/Event"
+import Login from "./pages/Login"
 
-function App() {
+export default function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
 
-export default App
